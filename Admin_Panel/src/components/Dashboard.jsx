4fde4e871391326d87065a776c616abb355ac43c
wilfredmutwiri <wilfredmutwiri20@gmail.com>
2025-06-16 09:55:45 +0300

    fix( Restyled admin dashboard)

--- Admin_Panel/src/components/Dashboard.jsx
+++ Admin_Panel/src/components/Dashboard.jsx
@@ -8,6 +8,7 @@ import { RiGasStationFill } from "react-icons/ri";
 import { FaShoppingCart } from "react-icons/fa";
 import { GoAlert } from "react-icons/go";
 import { FcApproval } from "react-icons/fc";
+import { CiNoWaitingSign } from "react-icons/ci";
 
 
 export default function Dashboard() {
@@ -113,10 +114,10 @@ const getWorkersCount=async()=>{
                     <Label className={`text-xs text-center font-semibold ${outdatedRecord ?"text-red-700":"text-gray-700" }`}>{outdatedRecord ? "Error fetching Stations!" : "All registered stations"}</Label>
                     </div>
 
-                    <div className='bg-gray-300 p-3 rounded-md flex-1 gap-4 text-black shadow-md hover:shadow-none shadow-gray-400'>
+                    <div className='bg-green-200 p-3 rounded-md flex-1 gap-4 text-black shadow-md hover:shadow-none shadow-gray-400'>
                         <FaShoppingCart className='text-xl mx-auto w-auto h-10 text-green-700'/>
                         <h2 className='font-semibold text-lg pt-3 text-center'>Total Orders</h2>
-                        <h3 className='text-4xl font-semibold pt-3 text-cyan-700 text-center'>{ordersCount || 0}</h3>
+                        <h3 className='text-4xl font-semibold pt-3 text-green-800 text-center'>{ordersCount || 0}</h3>
                         <h2 className='flex justify-between pt-3 gap-4'>
                         <p className={`text-xs font-semibold pt-2  ${outdatedRecord ? "text-red-700":"text-gray-700"}`}>{outdatedRecord ? "Error fetching orders!" : "Orders made via the app"}</p>
                         <Link to="/ManageTeachers">
@@ -125,10 +126,10 @@ const getWorkersCount=async()=>{
                         </h2>
                     </div>
 
-                    <div className='p-3 rounded-md flex-1 bg-gray-300 shadow-md hover:shadow-none shadow-gray-400'>
+                    <div className='p-3 rounded-md flex-1 bg-red-200 shadow-md hover:shadow-none shadow-gray-400'>
                         <GoAlert  className='text-xl mx-auto w-auto h-10 text-red-700'/>
                         <h2 className='font-semibold text-lg pt-3 text-black text-center'>Emergency Requests</h2>
-                        <h3 className='text-4xl text-center font-semibold pt-3 text-cyan-700'>{stationsCount}</h3>
+                        <h3 className='text-4xl text-center font-semibold pt-3 text-red-800'>{stationsCount}</h3>
                         <h2 className='flex justify-between pt-3 gap-4'>
                         <p className={`text-xs font-semibold ${outdatedRecord ? "text-red-700":"text-gray-700"}`}>{outdatedRecord ? "Outdated Record!" : "Pending Emergency Requests"}</p>
                         <Link to="/manageParents">
@@ -136,13 +137,28 @@ const getWorkersCount=async()=>{
                         </Link>
                         </h2>
                     </div>
-
-                    <div className='p-3 rounded-md flex-1 bg-gray-300 shadow-md hover:shadow-none shadow-gray-400'>
+                </div>
+                {/* second collection */}
+                <div className='flex flex-row gap-4 justify-between'>
+                    <div className='p-3 rounded-md flex-1 bg-green-200 shadow-md hover:shadow-none shadow-gray-400'>
                         <FcApproval className='text-xl mx-auto w-auto h-10 text-cyan-700'/>
                         <h2 className='font-semibold text-lg text-center pt-3 text-black'>Approved Stations</h2>
-                        <h3 className='text-4xl text-center font-semibold pt-3 text-cyan-700'>{stationsCount}</h3>
+                        <h3 className='text-4xl text-center font-semibold pt-3 text-green-800'>{stationsCount}</h3>
                         <h2 className='flex justify-between pt-3 gap-4'>
                         <p className={`text-xs pt-2 font-semibold ${outdatedRecord ? "text-red-700":"text-gray-700"}`}>{outdatedRecord ? "Outdated Record!" : "All approved stations"}</p>
+                        <Link to="/manageParents">
+                        <h2 className='text-sm cursor-pointer bg-white text-cyan-700 font-semibold p-2 rounded-md hover:bg-cyan-700 hover:text-white'>View</h2>
+                        </Link>
+                        </h2>
+                    </div>
+
+
+                    <div className='p-3 rounded-md flex-1 bg-orange-200 shadow-md hover:shadow-none shadow-gray-400'>
+                        <CiNoWaitingSign className='text-xl mx-auto w-auto h-10 text-yellow-500'/>
+                        <h2 className='font-semibold text-lg text-center pt-3 text-black'>Pending Stations Approvals</h2>
+                        <h3 className='text-4xl text-center font-semibold pt-3 text-yellow-500'>{stationsCount}</h3>
+                        <h2 className='flex justify-between pt-3 gap-4'>
+                        <p className={`text-xs pt-2 font-semibold ${outdatedRecord ? "text-red-700":"text-gray-700"}`}>{outdatedRecord ? "Outdated Record!" : "Stations awaiting approval"}</p>
                         <Link to="/manageParents">
                         <h2 className='text-sm cursor-pointer bg-white text-cyan-700 font-semibold p-2 rounded-md hover:bg-cyan-700 hover:text-white'>View</h2>
                         </Link>
