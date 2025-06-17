1dae6e5b48c51411ff16b7866125009c0f7cefad
wilfredmutwiri <wilfredmutwiri20@gmail.com>
2025-06-17 13:30:29 +0300

    update( Added route to delete a station )

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -4,7 +4,7 @@ const { userSignin, userSignout, getUserInfo } = require("./controllers/auth/use
 const { requestOTP } = require("./controllers/auth/requestOTP.js");
 const { resetPassword } = require("./controllers/auth/resetPassword.js");
 const { stationSignin } = require("./controllers/auth/stationSignin.js");
-const { stationSignup, getAllStations,getStationById, updateStationStatus, getAllApprovedStations, getAllNotApprovedStations} = require("./controllers/auth/stationSignup.js");
+const { stationSignup, getAllStations,getStationById, updateStationStatus, getAllApprovedStations, getAllNotApprovedStations, deleteStation} = require("./controllers/auth/stationSignup.js");
 const {fileUpload} = require("./controllers/auth/fileUpload.js");
 const {profileUpload,certUpload} = require("./middlewares/multer.js");
 const { placeOrder, getAllOrders, getOrderById, updateOrder, getOrdersByStation, getOrdersByCustomer, getDeliveredOrdersByStation, getCanceledOrdersByStation, getApprovedOrdersByStation, getTotalAmountByStation, getTotalVolumeDeliveredByStation, getOrdersByMonth } = require("./controllers/auth/order.js");
@@ -32,6 +32,7 @@ router.post('/station/signup/',stationSignup)
 router.get('/station/all',getAllStations)
 router.get('/station/:id',getStationById)
 router.patch('/station/update/:id',updateStationStatus);
+router.delete('/station/delete/:id',deleteStation);
 
 // file uploads
 router.post('/upload/images/',profileUpload.single('file'),fileUpload);
